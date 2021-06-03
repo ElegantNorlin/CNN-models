144feab3f902827c552e396fc88402eaf5884337
万万 <15863465252@163.com>
2021-06-03 22:13:11 +0800

    pytorch_offical_demo注释

--- alexnet/model.py
+++ alexnet/model.py
@@ -5,45 +5,73 @@ import torch
 class AlexNet(nn.Module):
     def __init__(self, num_classes=1000, init_weights=False):
         super(AlexNet, self).__init__()
+        # 卷积层的定义，具体的输入输出特征尺寸计算公式可以自行学习其他资料，利用好网络这把双刃剑
         self.features = nn.Sequential(
-            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
+            '''
+            nn.Conv2d(padding=2)其中的padding接收的参数可以是整型也可以是元组
+            如果是整形，那么相当于再图片的四周补上padding行0
+            如果是元组(a,b):
+                在图片的上下填充a行0
+                在图片的左右填充b列0
+            '''
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[96, 55, 55]
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[48, 27, 27]
-            nn.Conv2d(48, 128, kernel_size=5, padding=2),           # output[128, 27, 27]
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[96, 27, 27]
+            nn.Conv2d(96, 256, kernel_size=5, stride=1,padding=2),           # output[256, 27, 27]
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 13, 13]
-            nn.Conv2d(128, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[256, 13, 13]
+            nn.Conv2d(256, 384, kernel_size=3,stride=1, padding=1),          # output[384, 13, 13]
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.Conv2d(384, 384, kernel_size=3,stride=1,padding=1),          # output[384, 13, 13]
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 128, kernel_size=3, padding=1),          # output[128, 13, 13]
+            nn.Conv2d(384, 256, kernel_size=3,stride=1, padding=1),          # output[256, 13, 13]
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 6, 6]
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[256, 6, 6]
         )
+        # 全连接层的定义
         self.classifier = nn.Sequential(
+            # p=0.5代表神经元随机丢弃(失活)的比例
             nn.Dropout(p=0.5),
-            nn.Linear(128 * 6 * 6, 2048),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
-            nn.Linear(2048, 2048),
+            nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(2048, num_classes),
+            nn.Linear(4096, num_classes),
         )
+        # 是否初始化权重
         if init_weights:
+            # 调用初始化权重函数
             self._initialize_weights()
-
+    # Alexnet网络的前向传播过程
     def forward(self, x):
+        # 卷积层
         x = self.features(x)
+        # 特征拉直，然后输入全连接层
+        '''
+        假设类型为 torch.tensor 的张量 t 的形状如下所示：(2,4,3,5,6)，则 orch.flatten(t, 1, 3).shape 的结果为
+        (2, 60, 6)。将索引为 start_dim 和 end_dim 之间（包括该位置）的数量相乘，其余位置不变。
+        因为默认 start_dim=0，end_dim=-1，所以 torch.flatten(t) 返回只有一维的数据。
+        '''
         x = torch.flatten(x, start_dim=1)
+        # 将拉直的特征输入全连接层
         x = self.classifier(x)
         return x
-
+    # 初始化权重函数
     def _initialize_weights(self):
+        # self.modules()是一个迭代器
+        # 这里for循环是迭代模型中的对象
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
+            # 判断对象是否是卷积层
+            if isinstance(m   , nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                # 如果偏置项不为空，那么则
                 if m.bias is not None:
+                    # m.bias初始化为0
                     nn.init.constant_(m.bias, 0)
+            # 判断对象是否是全连接层
             elif isinstance(m, nn.Linear):
+                # 按照正态分布给权重初始化
                 nn.init.normal_(m.weight, 0, 0.01)
+                # m.bias初始化为0
                 nn.init.constant_(m.bias, 0)
---
+++ pytorch_official_demo/train.py
@@ -0,0 +1,103 @@
+import torch
+import torchvision
+import torch.nn as nn
+from model import LeNet
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+
+def main():
+    # 定义数据预处理操作
+    transform = transforms.Compose(
+        # transforms.ToTensor()将数据变成向量
+        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))将数据做标准化处理
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # 50000张训练图片
+    # 第一次使用时要将download设置为True才会自动去下载数据集
+    # root='./data'数据集要下载到哪里 
+    # train=True导入训练集样本
+    # transform=transform将数据集做transform中的预处理操作
+    # download=False表示数据集要下载，当数据集下载完之后，我们要把download的值修改为False
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True,  
+                                             download=True, transform=transform)
+    # num_workers=0这个参数windows下设置成别的参数可能会报错
+    # batch_size=36每一个bacth中包含36个数据
+    # shuffle=True打乱数据集
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
+                                               shuffle=True, num_workers=0)
+
+    # 10000张验证图片
+    # 第一次使用时要将download设置为True才会自动去下载数据集
+    # train=False时，导入测试集
+    val_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=False, transform=transform)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=5000,
+                                             shuffle=False, num_workers=0)
+    # 将数据集放到迭代器中
+    val_data_iter = iter(val_loader)
+    # val_image为测试集图片
+    # val_label为测试集标签
+    val_image, val_label = val_data_iter.next()
+    # 标签列表 
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    # 导入定义的LeNet类
+    net = LeNet()
+    # 定义损失函数
+    loss_function = nn.CrossEntropyLoss()
+    # 定义优化器
+    # net.parameters()需要训练的参数列表
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    # 迭代训练集（epoch为数据集迭代的次数）
+    for epoch in range(5):  # loop over the dataset multiple times
+        # 累加训练过程的损失
+        running_loss = 0.0
+        for step, data in enumerate(train_loader, start=0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            # 将输入输入到网络中得到输出
+            outputs = net(inputs)
+            # 损失函数loss_function(outputs, labels)
+            # outputs为网络预测的结果
+            # labels为数据的真是标签
+            loss = loss_function(outputs, labels)
+            # 损失函数反向传播
+            loss.backward()
+            # 优化器进行参数更新
+            optimizer.step()
+
+            # 信息打印的过程
+            # print statistics
+            # 累加到running_loss变量当中
+            running_loss += loss.item()
+            if step % 500 == 499:    # print every 500 mini-batches
+                # torch.no_grad()可以避免梯度重复计算
+                with torch.no_grad():
+                    outputs = net(val_image)  # [batch, 10]
+                    # 寻找输出特征中最大值的索引是多少，dim=1的意思是我们跳过batch，从索引为1的维度开始寻找
+                    # [1]我们只需要拿到最大值所在的索引，而不需要知道具体的数值
+                    predict_y = torch.max(outputs, dim=1)[1]
+                    # torch.eq(predict_y, val_label)将预测结果于数据的真是标签对比是否相等，返回1或0
+                    # sum()加起来就知道在本次训练过程中预测对了多少个样本
+                    accuracy = torch.eq(predict_y, val_label).sum().item() / val_label.size(0)
+
+                    # running_loss / 500为平均训练误差
+                    print('[%d, %5d] train_loss: %.3f  test_accuracy: %.3f' %
+                          (epoch + 1, step + 1, running_loss / 500, accuracy))
+                    # running_loss清零，准备进行下一轮数据集训练
+                    running_loss = 0.0
+
+    print('Finished Training')
+    # 将模型权重保存到设置的路径下
+    save_path = './Lenet.pth'
+    torch.save(net.state_dict(), save_path)
+
+
+if __name__ == '__main__':
+    main()
