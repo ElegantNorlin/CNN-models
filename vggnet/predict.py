2bb9f2fdc48ab0e8ef055bb7416ca541f34660df
万万 <15863465252@163.com>
2021-06-04 17:57:54 +0800

    update

--- vggnet/predict.py
+++ vggnet/predict.py
@@ -5,26 +5,33 @@ import torch
 from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
-
+# 引入定义的vgg类
 from model import vgg
 
 
 def main():
+    # 使用什么设备进行训练
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+    # 定义数据集预处理操作函数
     data_transform = transforms.Compose(
         [transforms.Resize((224, 224)),
          transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     # load image
+    # 添加预测图片的路径
     img_path = "../tulip.jpg"
+    # 验证图片路径是否存在
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    # 根据路径，打开图片
     img = Image.open(img_path)
+    # 展示图片
     plt.imshow(img)
     # [N, C, H, W]
+    # 调整图片维度顺序
     img = data_transform(img)
-    # expand batch dimension
+    # expand batch dimension扩展数据维度
+    # 这个函数可以看一下这个链接，讲的很清楚https://zhuanlan.zhihu.com/p/86763381
     img = torch.unsqueeze(img, dim=0)
 
     # read class_indict
